a8388443916eb14804432fd7e583d719c997a5e7
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2026-02-24 20:56:36 +0200

    teksts

--- lesson2.js
+++ lesson2.js
@@ -39,5 +39,34 @@ setInterval(function(){
 document.body.style.background = `rgb(${rgb.red},${rgb.green},${rgb.blue})`;
 },5000);
 
+// Funkcijas darbam ar tekstu (String)
+
+let text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
 
+
+
+let array1 = text.split('. '); //teksta sadalīšana pēc noteiktā simbola
+let shortTxt = text.slice(100,255)+"...";
+let upperLetters = text.toUpperCase();
+let searchText = text.search("1500ss");    
+
+
+
+console.log(text.length);
+console.log(array1);
+console.log(shortTxt.length,shortTxt);
+console.log(upperLetters);
+console.log(searchText);
+console.log(text.charAt(searchText));
+// Regulārā izteiksme
+
+let phoneRegex = /(\+371)([0-9]{8})/gm;
+let passworCheck = /^.*(?=.{8,120})(?!.*\s)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\!\@\#\$\%\^\&\*\(\)\-\=\¡\£\_\+\`\~\.\,\<\>\/\?\;\:\'\"\\\|\[\]\{\}]).*$/gm;
+
+phone = "+37122222222";
+
+console.log(phone.match(phoneRegex))
+console.log(shortTxt + " " + phone);
+
+// trim()
